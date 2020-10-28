01eff506e2f814af34dad38845938703af0fb1f8
Gustavo Isensee <gustavo.isensee@gmail.com>
2020-10-28 09:07:27 +0100

    refactor: add vueRouter on main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import 'es6-promise/auto';
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import store from './store';
 import router from './router';
 import Main from './components/Main';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
 
 new Vue({
   render: h => h(Main),
--- src/router.js
+++ src/router.js
@@ -1,20 +1,13 @@
-import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './components/App';
 
 const About = () => import('./components/About');
-
-Vue.use(VueRouter);
 
 const routes = [
   { path: '/', component: App },
   { path: '/about', component: About }
 ]
 
-const router = new VueRouter({
+export default new VueRouter({
   routes
 });
-
-
-
-export default router;

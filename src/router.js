cf19ec052f0f4ca74dd8e4b6f65c17b9a0e77bf5
Gustavo Isensee <gustavo.isensee@gmail.com>
2020-11-02 21:35:29 +0100

    fix: add history mode on router

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import App from './components/App';
 
 const routes = [
@@ -11,7 +11,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 });
 

cd3452b548698e3262e49aa954c1915dc51fa1a9
Gustavo Isensee <gustavo.isensee@gmail.com>
2020-11-02 21:45:48 +0100

    test: fix About component test

--- src/components/About/About.vue
+++ src/components/About/About.vue
@@ -4,11 +4,11 @@
     <div>
       <p>This Little project has the aim of creating a Todo list, where the user can add, edit, delete and mark them as done!</p>
       <strong>How does it work?</strong>
-      <ul class="list">
+      <ul class="list" data-testid="list">
         <li>Adding a new todo: type your todo in the input field and press enter.</li>
-        <li>Editing a todo: click twice in line of a todo, input field wil show up, so, edit it and press enter.</li>
+        <li>Editing a todo: click twice in the todo's row. An input field will show up, so, edit it and press enter.</li>
         <li>Deleting a todo: click at the delete button and confirm on the confirmation box.</li>
-        <li>Marking the todo as done: click at the checkbox or the label.</li>
+        <li>Marking the todo as done: click at the checkbox or in the label.</li>
       </ul>
     </div>
   </div>
--- src/components/__tests__/About.js
+++ src/components/__tests__/About.js
@@ -1,10 +1,14 @@
 import { render } from '@testing-library/vue';
 import About from '../About';
 
-describe('About', () => {
-  it('render the component properly', () => {
-    const { getByText } = render(About);
+describe('About component', () => {
+  it('renders the component properly', () => {
+    const { getByText, getByTestId } = render(About);
 
-    expect(getByText('About this little project!')).toBeTruthy();
+    expect(getByText(/add, edit, delete and mark them as done/i)).toBeInTheDocument();
+    expect(getByText(/About this little project!/)).toBeInTheDocument();
+
+    const list = getByTestId('list');
+    expect(list.children).toHaveLength(4);
   });
 });
